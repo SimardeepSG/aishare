0372c193007acefa92da87052ce0399fff601c36
SimardeepSG <88531239+SimardeepSG@users.noreply.github.com>
2024-07-02 23:46:14 -0700

    Navigation
    Navigation was added to the project and the following detailed changes are listed below:
    
    1) the entire codebase in layout.jsx was deleted and rnfes was used to create the used template
    2) added a profile.jsx file to test out linking
    3) created a link in index.jsx
    4) used 2 methods of routing in layout.jsx
    - a) slot component
    - b) stack component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//rnfes was used in tandem with ES7+ React/Redux/React-Native snippets v4.4.3 in order to create the general schema/format of the code below
 
-export default function App() {
+//layout.js is present in all navigation panes - so a nav bar or footer would be here
+//This is your main file where you define the overall structure of your application. 
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot, Stack } from 'expo-router'
+import React from 'react'
+
+const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>Simar's app. this is my app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false}} />
+      {/* header shown will  display the name of the screen, in this case inde was at the top*/}
+    </Stack>
+  )
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout
+
+// const styles = StyleSheet.create({
+// //new style class created in the container
+//   container: {
+//     display: 'flex',
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   }
+// })
+
+
+
+//Previous Notes
+
+//Here, you include the <slot /> component to specify where the child routes should be rendered.
+//slot will automatically render the default component aka index.js. If index is not provided it will not render anything
+//slot only renders child components and you need to specify them 
+
+
+// return (
+  //   <>
+  //     <Text> Header </Text>
+  //     <Slot />
+  //     <Text> Fooer </Text>
+  //   </>
+  //using slot renders the index.jsx file here onto layout.jsx
+  //reders current child route
+  //can be wrapped with diff components by adding into {}
+
+  // )
+//   return (
+//     <View style={styles.container}>
+//       <Text>RootLayout</Text>
+//     </View>
+//   )
---
+++ app/index.jsx
@@ -0,0 +1,29 @@
+//Links to pay attention to
+//https://www.youtube.com/watch?v=ek_IdGC0G80 - reactive native updates and general info 
+
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
+//allows custom routing tabs to be linked to another screen
+//INDEX.js is ALWAYS mapped to the main / directory or we call home directory
+//When you create a new folder in react and you put an index.js that is the default route that people will experience uncless you provide na links
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style = {{ color: 'blue' }}> Go to Profile </Link>
+      {/* in order to create links to different pages */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
