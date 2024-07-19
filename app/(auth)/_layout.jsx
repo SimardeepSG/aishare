39f0ea38fbc8b047dfc0bec58214f56f0d62abb3
SimardeepSG <88531239+SimardeepSG@users.noreply.github.com>
2024-07-19 14:22:17 -0700

    File and Folder Structure
    Creates all files and folders that wil be used for the project including different tab groups and layout structures
    
    also downloaded the web view version of the react native app so I can viewe it in the browser

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+// I ran RNFE for template layout file 
+
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  )
+}
+
+export default AuthLayout
